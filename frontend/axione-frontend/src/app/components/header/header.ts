31d1ae9c66c4f586aff1d95261afe3e7dc55bb2b
Aman Khansili <amankhansilee@gmail.com>
2026-07-21 15:42:06 +0530

    header implemented with hamburger

--- frontend/axione-frontend/src/app/components/header/header.ts
+++ frontend/axione-frontend/src/app/components/header/header.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  mobileMenuOpen = signal(false);
+  isScrolled = signal(false);
+
+  navItems = [
+    { label: 'Home', path: '/' },
+    { label: 'About', path: '/about' },
+    { label: 'Services', path: '/services' },
+    { label: 'Careers', path: '/careers' },
+    { label: 'Contact', path: '/contact' },
+  ];
+
+  toggleMenu() {
+    this.mobileMenuOpen.update((v) => !v);
+  }
+
+  closeMenu() {
+    this.mobileMenuOpen.set(false);
+  }
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.isScrolled.set(window.scrollY > 20);
+  }
+}
