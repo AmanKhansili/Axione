a0ec9d781d42f9015d3994ebd631b6824a7b9249
Aman Khansili <amankhansilee@gmail.com>
2026-07-23 17:52:28 +0530

    service section pop up done

--- frontend/axione-frontend/src/app/components/service-modal/service-modal.ts
+++ frontend/axione-frontend/src/app/components/service-modal/service-modal.ts
@@ -1,14 +1,16 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-service-modal',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './service-modal.html',
   styleUrl: './service-modal.scss',
 })
 export class ServiceModalComponent {
-  @Input() title = '';
+  @Input() service: any;
 
   @Output() close = new EventEmitter<void>();
 
--- frontend/axione-frontend/src/app/model/service.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Service {
-  id: string;
-  title: string;
-  icon: string;
-  tagline: string;
-  overview: string;
-  features: string[];
-  technologies: string[];
-}
